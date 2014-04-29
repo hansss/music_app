f704676b175abc7cbe13b1588847f0d26483f3aa
hansss <hanakim@college.harvard.edu>
2014-04-29 18:15:18 +0100

    Got audio player working.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,6 @@
 class Song < ActiveRecord::Base
-  attr_accessible :name, :artist_id, :comment_ids
+  attr_accessible :name, :artist_id, :comment_ids, :song_audio
+  mount_uploader :song_audio, SongAudioUploader
 
   has_many :comments
   belongs_to :artist
